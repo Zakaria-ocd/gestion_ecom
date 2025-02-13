5a0bd4f4cda49f5c0afdbf4d7f51766a93f976a6
Zakaria-ocd <zafrendo@gmail.com>
2025-02-13 21:33:01 +0100

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public  function boot()
 {
     Schema::defaultStringLength(191); // ✅ This fixes the key length issue
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    };
 }
 }
